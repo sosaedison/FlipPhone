e7780efb270a629afce9e80c8fa4bb60f7c34f8a
pike3633 <pike3633@stthomas.edu>
2018-05-30 12:33:41 -0500

    Pressable Class Created
    Dude...

---
+++ Pressable.java
@@ -0,0 +1,28 @@
+/**
+ * This class is the interface for the other classes in Lab2.
+ * 
+ * @author Zac Pike
+ *
+ */
+public class Pressable {
+	
+	/**
+	 * 
+	 * @author Zac Pike
+	 *
+	 */
+	public class Multitap{
+		
+	}
+	
+	/**
+	 * 
+	 * @author Sosa Edison
+	 *
+	 */
+	public class ReorderedMultitap{
+		
+	}
+	
+	
+}
